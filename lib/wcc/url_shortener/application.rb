439a2105237b6fdfd16eedd0b456c010984a4a5e
Gordon <gburgett@watermark.org>
2022-09-23 11:40:38 -0500

    Add additional default middleware

--- lib/wcc/url_shortener/application.rb
+++ lib/wcc/url_shortener/application.rb
@@ -2,9 +2,12 @@
 
 require 'rack/builder'
 require 'rack/static'
+require 'rack/common_logger'
+require 'rack/conditional_get'
+require 'rack/etag'
+require 'rack/deflater'
 
 class WCC::UrlShortener::Application
-
   def root
     # Project root - allows accessing config dir
     Pathname.new(File.expand_path(File.join(__dir__, '../../..')))
@@ -15,18 +18,23 @@ class WCC::UrlShortener::Application
   end
 
   def initialize
-    @app = Rack::Builder.new do
-      # Defaults
-      use Rack::Static, :urls => [""], :root => 'public', :index => 'index.html'
+    @app =
+      Rack::Builder.new do
+        # Defaults
+        use Rack::Deflater
+        use Rack::Head
+        use Rack::ConditionalGet
+        use Rack::ETag
+        use Rack::Static, urls: [''], root: 'public', index: 'index.html'
 
-      run ->(env) { [404, {}, ["Not Found: #{Rack::Request.new(env).path}"]] }
-    end
+        run ->(env) { [404, {}, ["Not Found: #{Rack::Request.new(env).path}"]] }
+      end
   end
 
   def prepare!
     raise StandardError, 'Cannot prepare twice!' if prepared?
 
-    Dir[root.join('config/initializers/*.rb')].sort.each { |f| require f }
+    Dir[root.join('config/initializers/*.rb')].each { |f| require f }
 
     @prepared = true
   end
