ecc7998248e106e1de78f23d85eab2ac90198a0f
Gordon <gburgett@watermark.org>
2022-09-25 11:33:07 -0500

    Configure bugsnag project root

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -4,6 +4,7 @@ require 'bugsnag'
 
 Bugsnag.configure do |config|
   config.api_key = ENV.fetch('BUGSNAG_API_KEY', nil)
+  config.project_root = WCC::UrlShortener.application.root.to_s
 end
 
 WCC::UrlShortener.application.middleware.insert_before WCC::UrlShortener::RedirectRouter,
